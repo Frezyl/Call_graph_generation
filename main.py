4226c1ae5befd70e433f7093d8fa1d83b18cd02f
Gergely Hevesi <hevesi.gergely@gmail.com>
2021-11-24 08:54:59 +0100

    add comments

--- main.py
+++ main.py
@@ -46,39 +46,43 @@ if __name__ == '__main__':
     logging.getLogger('angr').setLevel('CRITICAL')  # TODO set logger level to sensible value
 
     ubi_samples = []
+    """list of ubiquiti file names"""
 
-    with open(f"ubi_libraries.txt", "rt") as f_list:
+    with open(f"ubi_libraries.txt", "rt") as f_list:  # load ubiquiti file names with collected libraries
         for line in f_list:
             ubi_samples.append(line.split()[0])
+
     ubi_ARM = []
+    """list of ubiquiti ARM file names"""
 
     with open(f"ubi_ARM.txt", "rt") as ubi_ARM_f:
         for line in ubi_ARM_f:
             ubi_ARM.append(line.split()[0])
 
-    ubi_samples = [sample for sample in ubi_samples if sample in ubi_ARM]
+    ubi_samples = [sample for sample in ubi_samples if sample in ubi_ARM]  # filter for ARM samples
 
     dlink_samples = []
-    """list of dlink names"""
+    """list of dlink file names"""
 
-    with open(f"dlink_libraries.txt", "rt") as f_list:
+    with open(f"dlink_libraries.txt", "rt") as f_list:  # load dlink file names with collected libraries
         for line in f_list:
             dlink_samples.append(line.split()[0])
 
     dlink_ARM = []
+    """list of dlink ARM file names"""
 
     with open(f"dlink_ARM.txt", "rt") as dlink_ARM_f:
         for line in dlink_ARM_f:
             dlink_ARM.append(line.split()[0])
 
-    dlink_samples = [sample for sample in dlink_samples if sample in dlink_ARM]  # filter to those with library
+    dlink_samples = [sample for sample in dlink_samples if sample in dlink_ARM]  # filter for ARM samples
 
     tag_dict = dict()
     """tag dictionary for nodes"""
 
     malware_samples = list()
 
-    with open(f"malware.txt", "rt") as malware_samples_f:
+    with open(f"malware.txt", "rt") as malware_samples_f:  # load malware sample names
         for line in malware_samples_f:
             malware_samples.append(line.split()[0])
 
@@ -87,42 +91,31 @@ if __name__ == '__main__':
     print(f"{len(malware_samples)} malware samples loaded")
 
     with open(f"call_graphs.txt", "wt") as results:
+
         # number of graphs
         N = len(ubi_samples) + len(dlink_samples) + len(malware_samples)
         results.write(f"{N}\n")
+
         feat_dict = {}
         """dictionary for node feature tags"""
 
         path_to_libraries = f"../libraries/"
-
-        # limit = 50
-        # counter = 1
+        """path to dynamically linked libraries"""
 
         samples = list()
-        samples = ubi_samples + dlink_samples + malware_samples
+        """list of all samples"""
+
+        samples = ubi_samples + dlink_samples + malware_samples  # combine all sample names into 1 list
 
         print(f"{len(dlink_samples) + len(ubi_samples)} benign samples loaded")
         print(f"{len(malware_samples)} malware samples loaded")
 
-        # j = 0
-        # k = 0
-        # for i in range(limit):
-        #    if i % 2 == 0:
-        #        samples.append(dlink_samples[j])
-        #        j += 1
-        #    else:
-        #        samples.append(malware_samples[k])
-        #        k += 1
-
         exception_samples = list()
+        """collect samples where exceptions occurred"""
 
         samples = tqdm(samples)
         for sample in samples:
             samples.set_description("Processing samples")
-            # print(sample)
-            # if counter > limit:
-            #    break
-            # counter += 1
             try:
 
                 if sample in ubi_samples:
@@ -146,7 +139,7 @@ if __name__ == '__main__':
                                                                    use_sim_procedures=False)
                     """angr project"""
                 elif sample in malware_samples:
-                    fast_proj: angr.project.Project = angr.Project('../malware/' + sample, load_options={'auto_load_libs': False})  # TODO ask load options
+                    fast_proj: angr.project.Project = angr.Project('../malware/' + sample, load_options={'auto_load_libs': False})
 
                 fast: angr.analyses.cfg.cfg_fast.CFGFast = fast_proj.analyses.CFGFast()
                 """CFGFast analysis"""
@@ -169,9 +162,9 @@ if __name__ == '__main__':
                 """
 
                 if sample in malware_samples:
-                    graph_label = 0
+                    graph_label = 0  # set graph label to 0 for malware
                 elif sample in dlink_samples or ubi_samples:
-                    graph_label = 1
+                    graph_label = 1  # set graph label to 1 for benign
 
                 tag_dict = cfg_node_tagging(fast, tag_dict)
                 """extend node tag dictionary with new graph"""
@@ -179,8 +172,6 @@ if __name__ == '__main__':
                 # write number of nodes
                 n = networkx.number_of_nodes(fast_call_graph)
                 results.write(f"{n} {graph_label}\n")
-
-                node_count = networkx.number_of_nodes(fast_call_graph)
 
                 node_idx_dict = dict()
                 """node to index dictionary"""
@@ -191,19 +182,19 @@ if __name__ == '__main__':
                 # create node indices
                 node_idx_dict, idx_node_dict = node_index_generation(fast_call_graph, _node_idx_dict=node_idx_dict, _idx_node_dict=idx_node_dict)
 
-                for index in idx_node_dict:
-                    node = idx_node_dict[index]
+                for index in idx_node_dict:  # iterate over node indices
+                    node = idx_node_dict[index]  # get node from index
                     node_idx = node_idx_dict[node]
-                    memory_address = node
-                    node_tag = get_current_node_tag(addr=memory_address, _tag_dict=tag_dict)
+                    memory_address = node  # get memory address of node
+                    node_tag = get_current_node_tag(addr=memory_address, _tag_dict=tag_dict)  # get node tag
 
-                    number_of_neighbors = count_all_neighbors(_node=idx_node_dict[index])
-                    results.write(f"{node_tag} {number_of_neighbors}")
-                    write_neighbors_with_indices(fast_call_graph, node_idx_dict, idx_node_dict[node_idx])
+                    number_of_neighbors = count_all_neighbors(_node=idx_node_dict[index])  # get number of neighbours
+                    results.write(f"{node_tag} {number_of_neighbors}")  # write number of neighbours to file
+                    write_neighbors_with_indices(fast_call_graph, node_idx_dict, idx_node_dict[node_idx])  # write neighbour indices to file
 
-            except Exception as e:
+            except Exception as e:  # catch exception
                 print(e)
-                exception_samples.append(sample + "\n")
+                exception_samples.append(sample + "\n")  # append file name to exception list
 
     print(f"{len(exception_samples)} exceptions occured")
     with open("exceptions.txt", "wt") as exception_samples_f:
